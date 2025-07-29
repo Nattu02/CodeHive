75f8219d309cd4c1d1fb45efb42606df1824b94a
Nattudurai <nattuduraic.02@gmail.com>
2025-07-29 21:41:24 +0530

    explored HTTP methods

--- src/app.js
+++ src/app.js
@@ -1,6 +1,35 @@
 const express = require("express");
 const app = express();
 
+app.use("/hello", (req, res) => {
+  res.send("Hello hello hello...");
+});
+
+app.use("/test2/2", (req, res) => {
+  res.send("this is test2 route");
+});
+
+app.use("/test", (req, res) => {
+  res.send("this is test route");
+});
+
+app.get("/user", (req, res) => {
+  res.send({
+    fname: "Nattudurai",
+    lname: "C",
+  });
+});
+
+app.post("/user", (req, res) => {
+  res.send("User data created successfully");
+});
+app.put("/user", (req, res) => {
+  res.send("User data updated successfully");
+});
+app.delete("/user", (req, res) => {
+  res.send("User data deleted successfully");
+});
+
 app.use("/", (req, res) => {
   res.send("reponse sent from home page...");
 });
