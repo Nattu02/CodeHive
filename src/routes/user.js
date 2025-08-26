b2eaf47a9bcd9ead1a0685863c7efd79e5382ee2
Nattudurai <nattuduraic.02@gmail.com>
2025-08-27 00:28:34 +0530

    pagination using .skip().limit()

--- src/routes/user.js
+++ src/routes/user.js
@@ -58,6 +58,12 @@ userRouter.get("/user/connections", userAuth, async (req, res) => {
 
 userRouter.get("/feed", userAuth, async (req, res) => {
   try {
+    let page = req.query.page;
+    let limit = req.query.limit;
+    limit = limit < 50 ? limit : 50;
+
+    const skip = (page - 1) * limit;
+
     const loggedInUser = req.user;
     const connectionRequests = await ConnectionRequest.find({
       $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
@@ -75,7 +81,10 @@ userRouter.get("/feed", userAuth, async (req, res) => {
         { _id: { $nin: Array.from(hiddenUsers) } },
         { _id: { $ne: loggedInUser._id } },
       ],
-    }).select("firstName lastName");
+    })
+      .select("firstName lastName")
+      .skip(skip)
+      .limit(limit);
 
     res.send({ feed });
   } catch (err) {
